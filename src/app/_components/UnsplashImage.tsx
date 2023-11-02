cacbc1bea7ad108d02e84450e1792e08856b4a56
Carl Fremault <carlfremault@yahoo.com>
2023-11-02 20:10:35 +0100

    removed legacy props from UnsplashImage component

--- src/app/_components/UnsplashImage.tsx
+++ src/app/_components/UnsplashImage.tsx
@@ -4,19 +4,29 @@ import Image from "next/image";
 const UnsplashImage = ({
   className,
   src,
+  height,
+  width,
   alt,
   photographerHref,
   photographerName,
 }: {
   className: string;
   src: string;
+  height: number;
+  width: number;
   alt: string;
   photographerHref: string;
   photographerName: string;
 }) => {
   return (
     <div className={`relative ${className}`}>
-      <Image src={src} alt="" layout="fill" objectFit="cover" />
+      <Image
+        src={src}
+        alt=""
+        className="h-full w-full object-cover"
+        height={height}
+        width={width}
+      />
       <p className="absolute bottom-0 left-0 origin-top-left -rotate-90 text-sm text-white">
         Photo by{" "}
         <a className="underline" href={photographerHref}>
