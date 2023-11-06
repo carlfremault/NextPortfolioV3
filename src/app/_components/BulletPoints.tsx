80e2af3e3e11090855c6e0bde374d904aee9dd99
Carl Fremault <carlfremault@yahoo.com>
2023-11-06 21:18:00 +0100

    created Bulletpoints & ImageGrid components

---
+++ src/app/_components/BulletPoints.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const BulletPoints = ({
+  bulletpoints,
+  className,
+}: {
+  bulletpoints: BulletPoint[];
+  className?: string;
+}) => {
+  return (
+    <ul className={`ms-6 list-outside list-disc space-y-4 ${className}`}>
+      {bulletpoints.map((bp) => (
+        <li key={bp.title}>
+          <span className="font-bold">{bp.title}:</span> {bp.text}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default BulletPoints;
