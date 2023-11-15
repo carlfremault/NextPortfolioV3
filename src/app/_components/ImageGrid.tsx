5ebd03a62fef635fa408041002962e04a0d5f787
Carl Fremault <carlfremault@yahoo.com>
2023-11-15 22:17:20 +0100

    modified modal carousel
    - navigate on click
    - navigate and exit with arrows and esc
    - open carousel with image clicked on
    - close window icon

--- src/app/_components/ImageGrid.tsx
+++ src/app/_components/ImageGrid.tsx
@@ -2,20 +2,52 @@
 
 import React, { useState } from "react";
 import Image from "next/image";
-import ImageModal from "./ImageModal";
+import ModalCarousel from "./ModalCarousel";
 
 const ImageGrid = ({ images }: { images: GridImage[] }) => {
   const [showModal, setShowModal] = useState(false);
+  const [indexShown, setIndexShown] = useState(0);
+
   const toggleModal = (e: any) => {
     e.stopPropagation();
+    setIndexShown(+e.target.dataset.index);
     setShowModal((prev) => !prev);
   };
 
+  const handleImageClick = (e: any) => {
+    e.stopPropagation();
+    setIndexShown((prev) => (prev + 1) % images.length);
+  };
+
+  const handleKeyDown = (e: any) => {
+    e.stopPropagation();
+    if (e.key === "Escape") {
+      setShowModal(false);
+    }
+    if (["ArrowLeft", "ArrowRight"].includes(e.key)) {
+      setIndexShown(
+        (prev) => (e.key === "ArrowLeft" ? prev - 1 : prev + 1) % images.length,
+      );
+    }
+  };
+
   return (
-    <div onClick={toggleModal}>
-      <ImageModal images={images} toggle={toggleModal} showModal={showModal} />
+    <div
+      onClick={toggleModal}
+      onKeyDown={handleKeyDown}
+      role="button"
+      tabIndex={0}
+      className="cursor-pointer"
+    >
+      <ModalCarousel
+        images={images}
+        toggle={toggleModal}
+        showModal={showModal}
+        handleImageClick={handleImageClick}
+        indexShown={indexShown}
+      />
       <div className="mb-20 columns-1 gap-2 md:columns-2">
-        {images.map((image) => (
+        {images.map((image, index) => (
           <Image
             className="mb-2 w-full"
             key={image.src}
@@ -23,6 +55,7 @@ const ImageGrid = ({ images }: { images: GridImage[] }) => {
             alt={image.alt}
             height={image.height}
             width={image.width}
+            data-index={index}
           />
         ))}
       </div>
