632e0ffb7194369b25f475bcb21a7288ccbe78e4
carlfremault <carlfremault@yahoo.com>
2025-10-08 17:13:37 +0200

    tweaking bio content

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
       <div className="col-span-1 md:col-span-3">
         <p className="indent-8">
           <span className="font-serif text-6xl">Hello,</span> I&apos;m Carl — a
-          full-stack developer with a French technical degree in programming and
-          three years of experience in front-end and full-stack development.
+          full-stack developer with a degree in software development and three
+          years of hands-on experience in building web applications.
         </p>
       </div>
       <div className="w-100 col-span-1 flex min-h-96 justify-center md:justify-start">
@@ -23,20 +23,24 @@ export default function Home() {
       </div>
       <div className="col-span-1 space-y-8 md:col-span-2">
         <p>
-          I first discovered web development during the COVID-19 pandemic, when
-          I transitioned from hospitality and sales-support to pursue a career
-          in tech. Since then, I&apos;ve been driven by a love for coding,
-          digital innovation, and continuous learning.
+          Web development captured my interest during the COVID-19 pandemic,
+          when I transitioned from hospitality and sales-support to pursue a
+          career in tech. Since then, a love for coding, digital innovation, and
+          continuous learning has guided my journey.
         </p>
         <p>
-          I&apos;m passionate about building technology that truly matters —
-          applications that improve lives, support communities, and create
-          positive change. I want my work to have a meaningful impact, helping
-          people access resources, connect, and thrive in ways that matter most.
+          I want to create solutions with real impact — applications that
+          improve lives, support communities, and create positive change. My
+          goal is for my work to help people and organizations operate more
+          efficiently, sustainably, or inclusively.
         </p>
       </div>
       <div className="col-span-1 md:col-span-3">
-        <p>Let&apos;s connect and shape the digital future together!</p>
+        <p>
+          Eager to take on new challenges, grow my expertise, and contribute to
+          projects that make a real difference, I look forward to connect and
+          shape the digital future together!
+        </p>
       </div>
     </div>
   );
